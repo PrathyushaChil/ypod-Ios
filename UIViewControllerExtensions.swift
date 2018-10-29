9b927b71c9c28a158823358b58293daff47fb26e
PrathyushaChil <preethichiluveru@gmail.com>
2018-10-29 15:20:09 +0530

    Add files via upload

---
+++ UIViewControllerExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerExtensions.swift
+//  YPO-iOS
+//
+//  Created by Abhilash Donepudi on 10/12/18.
+//  Copyright © 2018 YPO. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    class func loadFromNib<T: UIViewController>() -> T {
+        return T(nibName: String(describing: self), bundle: nil)
+    }
+}
+
